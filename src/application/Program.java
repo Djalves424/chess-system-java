72bc0ac015b81071817083de7df1642cd47959a4
Diego Alves <djalves424@gmail.com>
2023-01-25 21:32:21 -0300

    Fist class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boargame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello word");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
