26648828f80ea2646ba1cfa108f5e428a7a813ce
Diego Alves <djalves424@gmail.com>
2023-02-05 00:36:17 -0300

    Knight

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -5,8 +5,8 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Bishop extends ChessPiece{
-	
+public class Bishop extends ChessPiece {
+
 	public Bishop(Board board, Color color) {
 		super(board, color);
 	}
@@ -23,7 +23,7 @@ public class Bishop extends ChessPiece{
 		Position p = new Position(0, 0);
 		
 		// nw
-		p.setValues(position.getRow() - 1, position.getColumn()- 1);
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setValues(p.getRow() - 1, p.getColumn() - 1);
@@ -33,7 +33,7 @@ public class Bishop extends ChessPiece{
 		}
 		
 		// ne
-		p.setValues(position.getRow() - 1 , position.getColumn() + 1);
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setValues(p.getRow() - 1, p.getColumn() + 1);
