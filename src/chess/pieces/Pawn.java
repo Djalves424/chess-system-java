e5f8ae88f0f524b2b19810a6844eecea258ab8d6
Diego Alves <djalves424@gmail.com>
2023-02-05 22:20:12 -0300

    Special move - Promotion

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -43,11 +43,11 @@ public class Pawn extends ChessPiece{
 			// #specialmove en passant white
 			if (position.getRow() == 3) {
 				Position left = new Position(position.getRow(), position.getColumn() - 1);
-				if (getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().piece(left) == chessMatch.enPassantVulnerable()) {
+				if (getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
 					mat[left.getRow() - 1][left.getColumn()] = true;
 				}
 				Position right = new Position(position.getRow(), position.getColumn() + 1);
-				if (getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.enPassantVulnerable()) {
+				if (getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
 					mat[right.getRow() - 1][right.getColumn()] = true;
 				}
 			}
@@ -74,11 +74,11 @@ public class Pawn extends ChessPiece{
 			// #specialmove en passant black
 			if (position.getRow() == 4) {
 				Position left = new Position(position.getRow(), position.getColumn() - 1);
-				if (getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().piece(left) == chessMatch.enPassantVulnerable()) {
+				if (getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
 					mat[left.getRow() + 1][left.getColumn()] = true;
 				}
 				Position right = new Position(position.getRow(), position.getColumn() + 1);
-				if (getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.enPassantVulnerable()) {
+				if (getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
 					mat[right.getRow() + 1][right.getColumn()] = true;
 				}
 			}
@@ -88,6 +88,6 @@ public class Pawn extends ChessPiece{
 	
 	@Override
 	public String toString() {
-		return "p";
+		return "P";
 	}
 }
